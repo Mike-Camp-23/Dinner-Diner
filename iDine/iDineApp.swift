8e64f900adf9aaa5bf7c4daff55d566569c21684
Mike Camp <mike_camp23@icloud.com>
2021-07-09 11:24:56 -0400

    Initial Commit

---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Michael Camp on 7/9/21.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
